79d7de210fa8d3d37596c1a4e4539d40cbd748ed
geraldo <geeferreirasd034@gmail.com>
2020-09-10 13:43:35 -0300

    creation of the pet table

--- mypet/core/models.py
+++ mypet/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Pet(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    city = models.CharField(max_length=100)
+    phone = models.CharField(max_length=11)
+    email = models.EmailField()
+    active = models.BooleanField(default=True)
+    description = models.TextField()
+    begin_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
