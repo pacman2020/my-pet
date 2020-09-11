cf1cb011353944fce4fd208cf0929cd5b0050f10
geraldo <geeferreirasd034@gmail.com>
2020-09-11 09:06:49 -0300

    pet search and listing function

--- mypet/core/forms.py
+++ mypet/core/forms.py
@@ -7,12 +7,3 @@ class PetForm(forms.ModelForm):
         fields = (
             'user_id','city','phone','email','description','photo',
             )
-
-
-# user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-#     city = models.CharField(max_length=100)
-#     phone = models.CharField(max_length=11)
-#     email = models.EmailField()
-#     active = models.BooleanField(default=True)
-#     description = models.TextField()
-#     photo = models.ImageField(upload_to='media', null=True)
--- mypet/core/views.py
+++ mypet/core/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Pet
+from django.core.paginator import Paginator
 
 # Create your views here.
+def home(request):
+    if request.method == 'POST':
+        search_pet = request.POST.get('search')
+
+        if search_pet:
+            pet_list = Pet.objects.filter(city_icontains=search_pet)
+            paginator = Paginator(pet_list, 8)
+            page = request.GET.get('page')
+            data = {'pets': paginator.get_page(page)}
+
+            return render(request, 'pet/home.html', data)
+
+    pet_list = Pet.objects.all().order_by('-begin_date')
+    paginator = Paginator(pet_list, 8)
+    page = request.GET.get('page')
+    data = {'pets': paginator.get_page(page)}
+    return render(request, 'pet/home.html', data)
