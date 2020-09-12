01aa428cff59201279206c7d2f60fdbec81c0181
geraldo <geeferreirasd034@gmail.com>
2020-09-12 08:13:39 -0300

    search filter pet list, and my pets

--- mypet/core/urls.py
+++ mypet/core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('detail/<int:pk>', views.detail_pet, name='detail_pet'),
+    path('my_pets', views.my_pets, name='my_pets'),
 ]
--- mypet/core/views.py
+++ mypet/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Pet
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 def home(request):
@@ -8,7 +9,7 @@ def home(request):
         search_pet = request.POST.get('search')
 
         if search_pet:
-            pet_list = Pet.objects.filter(city_icontains=search_pet)
+            pet_list = Pet.objects.filter(city=search_pet)
             paginator = Paginator(pet_list, 8)
             page = request.GET.get('page')
             data = {'pets': paginator.get_page(page)}
@@ -16,6 +17,27 @@ def home(request):
             return render(request, 'pet/home.html', data)
 
     pet_list = Pet.objects.all().order_by('-begin_date')
+    paginator = Paginator(pet_list, 8)
+    page = request.GET.get('page')
+    data = {'pets': paginator.get_page(page)}
+    return render(request, 'pet/home.html', data)
+
+def my_pets(request):
+    if request.method == 'POST':
+        search_pet = request.POST.get('search')
+
+        if search_pet:
+            pet_list = Pet.objects.filter(
+                Q(city=search_pet),
+                Q(user_id=request.user.id)
+                )
+            paginator = Paginator(pet_list, 8)
+            page = request.GET.get('page')
+            data = {'pets': paginator.get_page(page)}
+
+            return render(request, 'pet/home.html', data)
+
+    pet_list = Pet.objects.all().order_by('-begin_date').filter(user_id=request.user.id)
     paginator = Paginator(pet_list, 8)
     page = request.GET.get('page')
     data = {'pets': paginator.get_page(page)}
